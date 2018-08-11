5be50c0d07fb130a48d596cfc330fd3bbeb8d578
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-08-11 19:37:42 +0200

    Generate color for board item and add initial controls

--- src/components/boards/BoardItem.vue
+++ src/components/boards/BoardItem.vue
@@ -1,10 +1,16 @@
 <template>
-  <div class="board-item">
-    {{board.name}}
+  <div class="board-item" :style="cardColor">
+    {{ board.name }}
+
+    <div class="board-item__controls">
+      <Icon type="ios-star" />
+    </div>
   </div>
 </template>
 
 <script>
+import { colorFromText } from '@/helpers/colors'
+
 export default {
   name: 'board-item',
   props: {
@@ -12,10 +18,35 @@ export default {
       type: Object,
       default: () => ({})
     }
+  },
+
+  computed: {
+    cardColor() {
+      return `background: ${colorFromText(this.board.name)}`
+    }
   }
 }
 </script>
 
 <style lang="sass" scoped>
+.board-item
+  position: relative
+  height: 8em
+  border-radius: 0.2em
+  font-size: 1.8em
+  padding: 0.5em
+  line-height: 1
 
+  &__controls
+    position: absolute
+    top: 0.5em
+    right: 0.5em
+    cursor: pointer
+
+    i
+      color: #8c8c8c
+      transition: all 0.2s ease-in-out
+
+      &:hover
+        color: #138eff
 </style>
