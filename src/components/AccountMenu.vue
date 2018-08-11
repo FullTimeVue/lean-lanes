868650e293c249a6c49091e14bc91cc1e4027678
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-08-11 18:36:18 +0200

    Define props for AccountMenu and styles

--- src/components/AccountMenu.vue
+++ src/components/AccountMenu.vue
@@ -1,9 +1,31 @@
 <template>
-  <div class="account-menu">Me</div>
+  <div class="account-menu" >
+    <Avatar :src="user.avatar" size="large"/>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'account-menu'
+  name: 'account-menu',
+  props: {
+    user: {
+      type: Object,
+      default: () => ({})
+    }
+  }
 }
 </script>
+
+<style lang="sass" scoped>
+.account-menu
+  position: fixed
+  top: 1em
+  right: 1em
+  display: inline-block
+  width: 3.7em
+  height: 3.7em
+  background: #fff
+  border-radius: 50%
+  box-shadow: 0 0 0.2em #ddd
+  padding: 0.2em 0.8em 0.2em 0.2em
+</style>
