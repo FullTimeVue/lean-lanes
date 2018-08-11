53b239650f1c02d0e6e95a0a97703cef93e12a0e
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-08-11 14:11:09 +0200

    Prepare tests for NavigationBar and Home page

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -4,6 +4,7 @@ import App from '@/App.vue'
 import i18n from '@/locale'
 
 const localVue = createLocalVue()
+
 localVue.use(Router)
 
 describe('App.vue', () => {
@@ -15,6 +16,9 @@ describe('App.vue', () => {
     router
   })
 
+  // Probably it makes no sense to test it this way
+  // but once App.vue will have more duties we can
+  // add related test cases.
   it('renders entry page', () => {
     expect(wrapper.contains('#app')).toBe(true)
   })
