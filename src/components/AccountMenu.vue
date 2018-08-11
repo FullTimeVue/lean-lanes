735cae81a73ee42b822a327d1eae935cd43f74f8
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-08-11 17:33:00 +0200

    Create AccountMenu component

---
+++ src/components/AccountMenu.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="account-menu">Me</div>
+</template>
+
+<script>
+export default {
+  name: 'account-menu'
+}
+</script>
--- src/helpers/registerComponents.js
+++ src/helpers/registerComponents.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
-import Logo from '@/components/Logo'
-import NavBar from '@/components/NavBar'
+import Logo from '@/components/Logo.vue'
+import NavBar from '@/components/NavBar.vue'
+import AccountMenu from '@/components/AccountMenu.vue'
 
 export default () => {
   Vue.component('logo', Logo)
   Vue.component('nav-bar', NavBar)
+  Vue.component('account-menu', AccountMenu)
 }
