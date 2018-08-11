e2af16ec18e8f4308c71f7fb28c4a4470e351c9f
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-08-11 15:18:02 +0200

    Rename NavigationBar -> NavBar

---
+++ tests/unit/NavBar.spec.js
@@ -0,0 +1,28 @@
+import { mount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+
+import i18n from '@/locale'
+import NavBar from '@/components/NavBar.vue'
+
+const localVue = createLocalVue()
+
+localVue.use(Vuex)
+
+describe('NavBar.vue', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = mount(NavBar, {
+      i18n,
+      localVue
+    })
+  })
+
+  it('renders navigation bar', () => {
+    expect(wrapper.contains('.nav')).toBe(true)
+  })
+
+  it('renders navigation bar with menu items', () => {
+    expect(wrapper.contains('.nav__menu')).toBe(true)
+  })
+})
