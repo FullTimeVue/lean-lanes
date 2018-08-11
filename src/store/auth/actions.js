ac59a8fd8ea6045be3b886c0fc2feed50be47944
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-08-11 11:25:47 +0200

    Add initial actions and mutations to Vuex auth module

--- src/store/auth/actions.js
+++ src/store/auth/actions.js
@@ -1,3 +1,11 @@
-export default {
+import { SAVE_TOKEN } from './action-types'
 
+export default {
+  login({commit}, onSuccess) {
+    commit(SAVE_TOKEN, 'dummy-token')
+
+    setTimeout(() => {
+      onSuccess()
+    }, 2000)
+  }
 }
--- src/store/auth/mutations.js
+++ src/store/auth/mutations.js
@@ -1,3 +1,9 @@
-export default {
+import { SAVE_TOKEN } from './action-types'
 
+export default {
+  [SAVE_TOKEN] (state, token) {
+    state.token = token
+
+    localStorage.setItem('token', 'dummy-token')
+  }
 }
