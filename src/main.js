9698f47b095a585d0cebbef1f189131fab27d628
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-08-11 11:24:42 +0200

    Use i18n module

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import i18n from './locale'
 import registerComponents from '@/helpers/registerComponents'
 
 import './registerServiceWorker'
@@ -17,5 +18,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
