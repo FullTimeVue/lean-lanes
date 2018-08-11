1b941370b5702bf7d74e8a1e76157779f074cb34
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-08-11 11:27:19 +0200

    Use i18n in global setup for Jest

--- tests/jest.setup.js
+++ tests/jest.setup.js
@@ -1,7 +1,9 @@
 import iView from 'iview/dist/iview.min'
 import Vue from 'vue'
 import registerComponents from '@/helpers/registerComponents'
+import VueI18n from 'vue-i18n'
 
 Vue.use(iView)
+Vue.use(VueI18n)
 
 registerComponents()
