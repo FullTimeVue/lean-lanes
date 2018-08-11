32059f2b5f51975758ec214096197622a0880019
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-08-11 17:14:35 +0200

    Add stub data for boards

--- src/store/boards/initial-state.js
+++ src/store/boards/initial-state.js
@@ -1,3 +1,28 @@
 export default {
+  boards: [
+    {
+      id: 'id-1',
+      name: 'Random Board'
+    },
 
+    {
+      id: 'id-2',
+      name: 'Awesome Board'
+    },
+
+    {
+      id: 'id-3',
+      name: 'Awesome Boardy'
+    },
+
+    {
+      id: 'id-4',
+      name: 'Fantastic Board'
+    },
+
+    {
+      id: 'id-5',
+      name: 'Incredible Board'
+    }
+  ]
 }
