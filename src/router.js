dabfe6e692ade7efb256d2c5a2f2536f51eb80e9
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-08-11 17:45:00 +0200

    Lazy-load Home and configure chunk name

--- src/router.js
+++ src/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import { LoadingBar } from 'iview'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -15,15 +14,15 @@ const router = new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "home" */ './views/Home.vue')
     },
 
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     },
 
