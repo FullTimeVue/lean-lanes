b33b2dd92ad5ee3085b9376f0d25ae80f1024fd4
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-08-12 21:24:34 +0200

    Update tests from Home

--- tests/unit/Home.spec.js
+++ tests/unit/Home.spec.js
@@ -1,6 +1,5 @@
-import { mount, createLocalVue } from '@vue/test-utils'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
-
 import storeStubs from '../stubs/store'
 import i18n from '@/locale'
 import Home from '@/views/Home.vue'
@@ -15,27 +14,15 @@ describe('Home.vue', () => {
 
   beforeEach(() => {
     store = new Vuex.Store(storeStubs)
-    wrapper = mount(Home, {
+    wrapper = shallowMount(Home, {
       i18n,
       store,
-      localVue,
-      stubs: ['router-link']
+      localVue
     })
   })
 
-  it('renders home page with navigation bar', () => {
-    expect(wrapper.contains('.home > .nav')).toBe(true)
-  })
-
+  // TODO: investigate on how to provide all dependencies to sub-components
   it('renders all available boards list', () => {
-    expect(wrapper.contains('.board-list')).toBe(true)
-  })
-
-  it('renders all available board items', () => {
-    expect(wrapper.contains('.board-item')).toBe(true)
-    expect(wrapper.findAll('.board-item').length).toBe(2)
-
-    // Get first board and check its name
-    expect(wrapper.find('.board-item').text()).toBe('Random Board')
+    expect(wrapper.contains('board-list-stub')).toBe(true)
   })
 })
